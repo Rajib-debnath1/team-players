8c53a82f29848d6f814ef6960fc7b33db8a7c613
Rajib-debnath1 <rajibdebnath519@gmail.com>
2023-03-30 14:54:03 +0600

    total price added

--- src/component/Information/Information.jsx
+++ src/component/Information/Information.jsx
@@ -3,12 +3,18 @@ import './Information.css'
 
 const Information = (props) => {
     const cart = props.cart;
+
+    let totalPrice = 0;
+    for(const player of cart){
+      totalPrice = totalPrice + player.price;
+      
+    }
     return (
         <div className='information'>
             <h4>Player Details</h4>
             <div>
-                <p>Name: </p>
-                <p>Total Price: $</p>
+                <p>Name:</p>
+                <p>Total Price: ${totalPrice}</p>
                 <p>Total Players: {cart.length}</p>
                 
             </div>
